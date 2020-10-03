74c9798f5e314af1bce9c147df8a3e879c08dd58
Glaver <xeron777@gmail.com>
2020-10-03 16:07:04 +0700

    add view model and loader for images

---
+++ TheMovieDataBase1.1/ViewModel/ImageViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ImageModelView.swift
+//  TheMovieDataBase1.1
+//
+//  Created by Vladyslav on 31/8/20.
+//  Copyright © 2020 Vladyslav Gubanov. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImageViewModel: View {
+    @ObservedObject var imageLoader: ImageLoaderViewModel
+    @State private var isAnimating = false
+    var imageName: String
+    
+    var body: some View {
+        ZStack {
+            if self.downloadAndStore(imageName: imageName) != nil {
+                Image(uiImage: self.downloadAndStore(imageName: imageName)!)
+                    .resizable()
+                    .renderingMode(.original)
+            } else {
+                Image("blur")
+                    .resizable()
+                    .renderingMode(.original)
+                ActivityIndicator(isAnimating: .constant(true), style: .large)
+            }
+        }
+    }
+    
+    func downloadAndStore(imageName: String) -> UIImage? {
+        if let loader = FileManager.fecthImage(imageName: imageName){
+            //print("upload from fileman \(imageName)")
+            return loader
+        }else{
+            var imageOutput: UIImage?
+            if let loaders = imageLoader.image {
+                imageOutput = loaders
+                if FileManager.saveImage(image: loaders, imageName: imageName) {
+                    //print("save to FileManager \(imageName)")
+                }
+            }
+            return imageOutput
+        }
+    }
+}
+
+
